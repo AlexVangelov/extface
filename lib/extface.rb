b9b3ae376f34147ebb4b8f4d8589ac8aeced084f
AlexVangelov <email@data.bg>
2014-02-22 11:20:40 +0200

    Redis connection string

--- lib/extface.rb
+++ lib/extface.rb
@@ -9,13 +9,17 @@ module Extface
   @@mappings = ActiveSupport::OrderedHash.new
   
   class << self
+    def setup
+      yield self
+    end
+
     def add_mapping(resource, options)
       mapping = Extface::Mapping.new(resource, options)
       @@mappings[mapping.name] = mapping
     end
     
     def redis_block
-      r = Redis.new
+      r = redis_instance
       begin
         yield r
       ensure
@@ -24,10 +28,12 @@ module Extface
     end
     
     private
-      def parse_redis_url
+      def redis_instance
         if @@redis_connection_string
           uri = URI.parse(@@redis_connection_string)
-          return {:host => uri.host, :port => uri.port, :password => uri.password}
+          Redis.new(host: uri.host, port: uri.port, password: uri.password)
+        else
+          Redis.new
         end
       end
   end 
