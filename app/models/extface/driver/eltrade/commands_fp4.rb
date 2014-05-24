5a5103ee27530bdff3e3b8664567a39a436fd989
AlexVangelov <email@data.bg>
2014-05-24 19:52:41 +0300

    Eltrade fiscal driver initial

--- app/models/extface/driver/eltrade/commands_fp4.rb
+++ app/models/extface/driver/eltrade/commands_fp4.rb
@@ -1,56 +1,58 @@
+#Source: ELTRADE COMMUNICATION PROTOCOL for Fiscal Printers (ver 4.0.2.12)
+
 module Extface
-  module Driver::Epson::Fiscal
-    STX = 0x02
-    ETX = 0x03
-    SEP = 0x1c
-    DC2 = 0x12
-    DC4 = 0x14
-    NAK = 0x15
+  module Driver::Eltrade::CommandsFp4
+    STRT = "\xAA\x55"
     
-    module Control
-      STATUS_IF             = 0x38
-      X_Z_REPORTS           = 0x39
-      FISC_MEM_REPORT_DATE  = 0x3a
-      FISC_MEM_REPORT       = 0x3b
+    module Info
+      GET_SERIAL_NUMBER     = 0x10
+      GET_HEADING_ROWS_1_6  = 0x11
+      GET_FISC_MEM_NUMBER   = 0x14
+      GET_TAX_NUMBER        = 0x15
+      GET_HEADING_ROWS_7_10 = 0x17
+      GET_TAX_GROUPS        = 0x19
+      GET_OPERATOR          = 0x1A
+      GET_PAYMENTS          = 0x1B
+      GET_PRINTER_STATUS    = 0x2C
+      GET_FISCAL_STATUS     = 0x30
+      GET_FISCAL_TYPE       = 0x3C
+      GET_DATE_TIME         = 0x3E
+      REQUEST_HARDWARE_STAT = 0x46
+      GET_HARDWARE_STATUS   = 0x47
+      GET_STATUS            = 0x70
     end
-    module Vouchers
-      OPEN_FISCAL_VOUCHER   = 0x40
-      PRINT_FISCAL_TEXT     = 0x41
-      PRINT_FISCAL_ARTICLE  = 0x42
-      SUBTOTAL_FISCAL       = 0x43
-      FISCAL_VOUCHER        = 0x44  # pay / cancel / discount in
-      CLOSE_FISCAL_VOUCHER  = 0x45
+    module Receipt
+      OPEN_RECEIPT          = 0x2D
+      START_RECEIPT         = 0x2E
+      CLOSE_RECEIPT         = 0x2F
+      LAST_DOC_COPY         = 0x31
+      SET_INVOICE_NUMBER    = 0x34
+      PRINT_RECEIPT_HEADER  = 0x38
     end
-    module NonFiscal
-      OPEN_NON_FISCAL_DOC   = 0x48
-      PRINT_NON_FISCAL_TEXT = 0x49
-      CLOSE_NON_FISCAL_DOC  = 0x4a
+    module Reports
+      SILENT0_PLU           = 0x22
+      SILENT_PRINTER        = 0x23
+      PLU_REPORT            = 0x32
+      DAILY_REPORT          = 0x33
     end
-    module Printer
-      CUT_PAPER         = 0x4b
-      ADVANCE_PAPER     = 0x50
-      ACTIVATE_SLIP     = 0xa0 #TMU950, TMU675
-      DISABLE_SLIP      = 0xa1
-      FORMAT_CHECKS     = 0xaa
-      FORMAT_ENDOSEMENT = 0xab
+    module Init
+      GRAPHIC_LOGO_PRINTING = 0x13
+      SET_OPERATOR_INFO     = 0x2A
+      SET_PAYMENT_TYPES     = 0x2B
+      SET_FISCAL_DEV_NUM    = 0x35
+      FISCAL_PARAMS_REC     = 0x36
+      SET_PRINT_PLU_NUMBER  = 0x3A
+      SET_SERIAL_NUMBER     = 0x3B
+      SET_DATE_TIME         = 0x3D
     end
-    module General
-      SET_DATE_HOUR   = 0x58
-      GET_DATE_HOUR   = 0x59
-      HEADED          = 0x5d
-      FOOT_OF_PAGE    = 0x5e
-      OPEN_DRAWER_1   = 0x7b
-      OPEN_DRAWER_2   = 0x7c
-    end
-    
-    def handle(buffer)
-      bytes_processed = 0
-      if frame_match = buffer.match(/\x02.*\x03.{4}/n)
-        frame_data = frame_match.to_s
-        rpush frame_data
-        bytes_processed = frame_match.pre_match.length + frame_data.length
-      end
-      return bytes_processed
+    module Other
+      EXEC_PRINTER_TEST   = 0x24
+      FISC_MEM_READ_TEST  = 0x25
+      FISC_MEM_WRITE_TEST = 0x26
+      PAYED_RECV_ACCOUNT  = 0x28  # ?
+      GET_DATE_TIME       = 0x3E
+      SET_DATE_TIME       = 0x3D
+      OPEN_CASH_DRAWER    = 0x4B
     end
 
     def build_packet(cmd, fields = [])
@@ -66,34 +68,5 @@ module Extface
         frame << bcc(frame)
       end
     end
-    
-    private
-      def bcc(buffer)
-        sum = 0
-        buffer.each_byte do |b|
-          sum += b
-        end
-        sum.to_s(16).rjust(4, '0')
-      end
-      
-      def sequence_number(increment = true)
-        @seq ||= 0x20
-        @seq += 1 if increment
-        @seq = 0x20 if @seq == 0x7f
-        @seq
-      end
-      
-      class Frame
-        include ActiveModel::Validations
-        attr_reader :frame, :seq, :cmd, :fields_data, :fields, :bcc
-
-        def initialize(buffer)
-          if match = buffer.match(/\x02([\x20-\x7F])([\x30-\xaf])\x1c(.*)\x03(.{4})/n)
-            @frame = match.to_a.first
-            @seq, @cmd, @fileds_data, @bcc = match.captures
-            @fields = @fields_data.split("\x1c")
-          end
-        end
-      end
   end
 end
